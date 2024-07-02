d4d6b964259e79a08cdfae3875ef475a11c78867
about-suntao <1550208137@qq.com>
2024-07-02 14:49:10 +0800

    fix 部署修改

--- src/app/life/menuTab/service/service.tsx
+++ src/app/life/menuTab/service/service.tsx
@@ -26,7 +26,7 @@ function Service() {
       <div className={styles.container}>
         <div className={styles.title}>
           <h2>生活服务</h2>
-          <p>Domestic services</p>
+          <p>Domestic Services</p>
         </div>
         <div className={styles.desc}>
           <p>我校膳食中心管理工作以规范化管理、标准化管理、热情服务、质量第一作为宗旨。每天供应新鲜蔬菜和水果，根据不同年龄段的孩子们成长中所需求的优质蛋白质、维生素、矿物质、膳食纤维等营养素，不断提高服务质量，严格操作规程。</p>
