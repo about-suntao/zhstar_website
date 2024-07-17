06761168f3a817f17366b4b6d2c491544b3e5643
about-suntao <1550208137@qq.com>
2024-07-17 16:01:32 +0800

    fix 休息表修改

--- src/app/education/menuTab/schedule/schedule.tsx
+++ src/app/education/menuTab/schedule/schedule.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import styles from './schedule.module.scss'
 import Image from 'next/image'
 
-import tableImg from '../../../../../public/img/education/schedule.webp'
-
 function Schedule() {
   return (
     <div className={styles.pages}>
       <div className={styles.container}>
         <div className={styles.table}>
           <Image src={'https://icon-zhuhai.oss-cn-hangzhou.aliyuncs.com/2024/07/17/114_schedule.webp'}
-            alt='' width={600} height={2000} priority></Image>
+            alt='' width={2000} height={5000} priority></Image>
         </div>
       </div>
     </div>
